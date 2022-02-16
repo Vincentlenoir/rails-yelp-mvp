9d542b0048125ca7b894336c83cff9506ace3c2f
Vincent Orinel <vincent.orinel@gmail.com>
2022-02-16 16:51:02 +0100

    k

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,7 +8,7 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
-    if review.save
+    if @review.save
       redirect_to restaurant_path(@restaurant)
     else
       render :new
